78a2d2b9f63deb0af4339f7be47790167ea4d9c5
Ahmed Imam <ahmedsamir_imam@hotmail.com>
2019-12-06 01:57:15 +0200

    commit

--- lib/parkingappbackend_web/router.ex
+++ lib/parkingappbackend_web/router.ex
@@ -15,13 +15,13 @@ defmodule ParkingappbackendWeb.Router do
     pipe_through :api
     post "/users/sign_up", UserController, :create
     post "/users/sign_in", UserController, :sign_in
-    get "/users", UserController, :index
+    #get "/users", UserController, :index
   end
 
   scope "/api", ParkingappbackendWeb do
     pipe_through [:api, :jwt_authenticated]
 
-    #get "/users", UserController, :index
+    get "/users", UserController, :index
     post "/users/update" , UserController, :update
     get "/my_account", UserController, :show
     post "/search", SearchController, :search_parkings
