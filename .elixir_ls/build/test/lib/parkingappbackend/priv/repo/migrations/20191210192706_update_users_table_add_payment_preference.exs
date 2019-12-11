de64607caeba8dbb2bb1a8b75a9db95ff1a8425e
Ahmed Imam <ahmedsamir_imam@hotmail.com>
2019-12-11 21:24:47 +0200

    pushing to gigalixir

--- .elixir_ls/build/test/lib/parkingappbackend/priv/repo/migrations/20191210192706_update_users_table_add_payment_preference.exs
+++
@@ -1,9 +0,0 @@
-defmodule Parkingappbackend.Repo.Migrations.UpdateUsersTableAddPaymentPreference do
-  use Ecto.Migration
-
-  def change do
-    alter table(:users) do
-      add :paymentpreference, :string, default: "EOP"
-    end
-  end
-end
